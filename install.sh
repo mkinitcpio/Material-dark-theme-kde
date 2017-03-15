b4c3502eee2f0af05fc7b033628751e68413bd00
mkinitcpio <wot220697@gmail.com>
2017-03-16 00:52:30 +0300

    Change common theme color

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cp --no-preserve=mode,ownership -r ./window-decorations/. ~/.local/share/aurorae/themes/material-dark/
+cp --no-preserve=mode,ownership -r ./theme/. ~/.local/share/plasma/desktoptheme/material-dark/
